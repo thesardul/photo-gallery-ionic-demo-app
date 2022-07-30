4ff5dd341eeb2e4818cab5ab5144e7bb5fecd96e
Sardul Yadav <27910681+thesardul@users.noreply.github.com>
2022-07-30 14:59:05 +0530

    Added Camera and File System configs

--- src/app/services/photo.service.ts
+++ src/app/services/photo.service.ts
@@ -1,9 +1,64 @@
 import { Injectable } from '@angular/core';
+import { Camera, CameraResultType, CameraSource, Photo } from '@capacitor/camera';
+import { Filesystem, Directory} from '@capacitor/filesystem';
+import { Storage } from '@ionic/storage-angular';
+
 
 @Injectable({
   providedIn: 'root'
 })
-export class PhotoService {
 
+
+export class PhotoService {
+  public photos : UserPhoto[] = [];
+  private PHOTO_STORAGE : string = 'photos';
+  public async addNewToGallery() {
+    // Take a photo
+    const capturedPhoto = await Camera.getPhoto({
+      resultType: CameraResultType.Uri, // file-based data; provides best performance
+      source: CameraSource.Camera, // automatically take a new photo with the camera
+      quality: 100 // highest quality (0 to 100)
+    });
+    const savedImageFile = await this.savePicture(capturedPhoto);
+    this.photos.unshift(savedImageFile);
+
+  }
+  private async savePicture(photo: Photo) {
+    // Convert photo to base64 format, required by Filesystem API to save
+    const base64Data = await this.readAsBase64(photo);
+
+    // Write the file to the data directory
+    const fileName = new Date().getTime() + '.jpeg';
+    const savedFile = await Filesystem.writeFile({
+      path: fileName,
+      data: base64Data,
+      directory: Directory.Data
+    });
+    return {
+      filepath: fileName,
+      webviewPath: photo.webPath
+    }
+  }
+  private async readAsBase64(photo: Photo) {
+    // Fetch the photo, read as a blob, then convert to base64 format
+    const response = await fetch(photo.webPath!);
+    const blob = await response.blob();
+
+    return await this.convertBlobToBase64(blob) as string;
+  }
+
+  private convertBlobToBase64 = (blob: Blob) => new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.onerror = reject;
+    reader.onload = () => {
+        resolve(reader.result);
+    };
+    reader.readAsDataURL(blob);
+  });
   constructor() { }
+}
+
+export interface UserPhoto {
+  filepath: string;
+  webviewPath: string;
 }
